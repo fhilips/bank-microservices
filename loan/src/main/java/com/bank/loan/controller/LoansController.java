f38a45c70c6ba0914299531b38264950be55d174
fhilips <fhilips.souza@gmail.com>
2021-12-22 01:01:26 -0300

    Loans microservice added

---
+++ loan/src/main/java/com/bank/loan/controller/LoansController.java
@@ -0,0 +1,33 @@
+
+package com.bank.loan.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bank.loan.model.Customer;
+import com.bank.loan.model.Loans;
+import com.bank.loan.repository.LoansRepository;
+
+
+@RestController
+public class LoansController {
+
+	@Autowired
+	private LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+
+	}
+
+}
