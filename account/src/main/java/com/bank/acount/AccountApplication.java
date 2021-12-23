408309491a85382f2df01b31cc1381dc3c0bc370
fhilips <fhilips.souza@gmail.com>
2021-12-23 11:42:38 -0300

    Changes in microservices config to register in eureka server

--- account/src/main/java/com/bank/acount/AccountApplication.java
+++ account/src/main/java/com/bank/acount/AccountApplication.java
@@ -2,8 +2,10 @@ package com.bank.acount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class AccountApplication {
 
 	public static void main(String[] args) {
